9e04c69775f916eda9fef32d726c48e226912bce
Alec <alec.lahr@gmail.com>
2021-04-29 23:39:30 -0400

    added obstacles script. updated cost to take images

--- src/terple_planning/scripts/cost.py
+++ src/terple_planning/scripts/cost.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python
+
 from math import sqrt, cos, sin, pi as PI
 import rospy
+from cv2 import line
 
 
-def cost(xn, yn, theta_deg, ul, ur, dt=0.1, tf=1):
+def curve(xn, yn, theta_deg, ul, ur, dt=0.1, tf=1, img=None, scl=None):
+    # img is an option arg. if present then func will also draw the curve on the img
+    # Must also pass scl with image. represents how much bigger the output viz video is than the grid
+
     r = rospy.get_param("/terple/robot_description/r")
     L = rospy.get_param("/terple/robot_description/L")
     GRID_D = rospy.get_param("/terple/space_description/GRID_D")
+    GRID_H = rospy.get_param("/terple/space_description/GRID_H")
 
-    # reformat to be in 10x10 grid system for calculations
+    # reformat to be in gazebo grid system for calculations
     xn /= float(GRID_D)
     yn /= float(GRID_D)
 
@@ -16,23 +23,32 @@ def cost(xn, yn, theta_deg, ul, ur, dt=0.1, tf=1):
     # draw 10 shorter lines to approximate the curve
     t = 0
     dist_traveled = 0
-    while t < tf - dt/2.0:
+    while t < tf - dt / 2.0:
         t = t + dt  # step the time by increment dt
-        xs = xn  # start point x - set as previous goal
-        ys = yn  # start point y - set as previous goal
+        xs = xn
+        ys = yn
 
+        # do the movement math
         dd = 0.5 * r * (ul + ur) * dt
         dxn = dd * cos(theta_rad)
         dyn = dd * sin(theta_rad)
         xn += dxn
         yn += dyn
         theta_rad += ((r / L) * (ur - ul) * dt)
-        dist_traveled += sqrt(dxn**2 + dyn**2)
+        dist_traveled += sqrt(dxn ** 2 + dyn ** 2)
 
+        if img:
+            img = line(img, (int(xs * scl * GRID_D), int(GRID_H * scl - ys * scl * GRID_D)),
+                       (int(xn * scl * GRID_D), int(GRID_H * scl - yn * scl * GRID_D)),
+                       (0, 0, 255))  # draw the small line, with scaled points
+
+    # convert back to working dimensions
     theta_deg = 180 * theta_rad / PI
     xn *= GRID_D
     yn *= GRID_D
     dist_traveled *= GRID_D
+
+    # return img only if one is given
+    if img:
+        return xn, yn, theta_deg, dist_traveled, img
     return xn, yn, theta_deg, dist_traveled
-
-
---
+++ src/terple_planning/scripts/obstacles.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import numpy as np
+from math import sqrt
+import rospy
+
+# get ros params that define board space
+ROBOT_RADIUS = rospy.get_param("/terple/robot_description/ROBOT_RADIUS")
+BOARD_H = rospy.get_param("/terple/space_description/BOARD_H")
+BOARD_W = rospy.get_param("/terple/space_description/BOARD_W")
+GRID_D = rospy.get_param("/terple/space_description/GRID_D")
+GRID_H = BOARD_H * GRID_D
+GRID_W = BOARD_W * GRID_D
+GRID_ROBOT_RADIUS = ROBOT_RADIUS * GRID_D
+
+
+# Board Obstacles
+quads = [[]]
+elips = [[]]
+
+
+def quad_check(x0, y0, quad):
+    # extract cords out of quad list
+    x1 = quad[0]
+    y1 = quad[1]
+    x2 = quad[2]
+    y2 = quad[3]
+    x3 = quad[4]
+    y3 = quad[5]
+    x4 = quad[6]
+    y4 = quad[7]
+
+    # check if the point is within the restricted half-plane side of each line
+    chk1 = line_check(x0, y0, x1, y1, x2, y2, False, False)
+    chk2 = line_check(x0, y0, x2, y2, x3, y3, False, True)
+    chk3 = line_check(x0, y0, x3, y3, x4, y4, True, True)
+    chk4 = line_check(x0, y0, x4, y4, x1, y1, True, False)
+
+    # check if point is within restricted half place side of all lines --> in object
+    return not (chk1 and chk2 and chk3 and chk4)  # if True, point is not in obstacle space
+
+
+def line_check(x0, y0, x1, y1, x2, y2, UD, LR):
+    # UD = True  if object is bottom side of line
+    # UD = False if object is top    side of line
+    # LR = True  if object is left   side of line
+    # LR = False if object is right  side of line
+    if x2 != x1:  # not vertical line
+        m = (y2 - y1) / float(x2 - x1)  # get the slope
+        b = y1 - m * x1  # get the intercept
+        # check if point is within the restricted half-plane
+        return (y0 >= m * x0 + b and not UD) or (
+                    y0 <= m * x0 + b and UD)  # if True, point is within the restricted half-plane
+    else:  # x2 == x1 --> vertical line
+        return (x0 >= x1 and not LR) or (x0 <= x1 and LR)  # if True, point is within the restricted half-plane
+
+
+def elip_check(x0, y0, elip):
+    # extract dimensions out of elip list
+    xc = elip[0]
+    yc = elip[1]
+    a2 = elip[2] ** 2  # horizontal dimension
+    b2 = elip[3] ** 2  # vertical dimension
+    return (x0 - xc) ** 2 / float(a2) + (y0 - yc) ** 2 / float(b2) > 1  # if True, point is in obstacle space
+
+
+def setup_graph(clearance, point_robot=True):
+    obst = np.ones((GRID_H, GRID_W))
+
+    # if no obstacles, then don't waste time looking for any
+    if quads == [[]] and elips == [[]]:
+        return obst
+
+    for x in range(GRID_W):
+        for y in range(GRID_H):
+            for quad in quads:  # check quads
+                if not quad_check(x, y, quad):  # see if point is near the quad
+                    obst[GRID_H - y - 1, x] = 0
+                    break
+
+            for elip in elips:  # check elips
+                if not elip_check(x, y, elip):  # see if point is near the elip
+                    obst[GRID_H - y - 1, x] = 0
+                    break
+
+    if not point_robot:  # used to override the expansion for the visualization step
+        return obst
+
+    newObst = np.ones((GRID_H, GRID_W))  # create new obstacle array that will have r
+    r = int(round(GRID_ROBOT_RADIUS + clearance))  # point robot radius
+    for x in range(GRID_W):
+        for y in range(GRID_H):
+            for i in range(x - r, x + r):  # window each pixel and check for an obstacle in radius
+                for j in range(y - r, y + r):
+                    if 0 <= i < GRID_W and 0 <= j < GRID_H:  # makes sure point is within bounds
+                        if obst[j, i] == 0 and sqrt((x - i) ** 2 + (y - j) ** 2) < r:  # if window point is in obstacle
+                            newObst[GRID_H - y, x] = 0
+                            break
+                else:
+                    continue
+                break
+
+    return newObst

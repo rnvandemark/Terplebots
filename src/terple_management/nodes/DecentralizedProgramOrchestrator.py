ab6bff4e263de7e6c7b11b7f90c9806b179df220
Nick Vandemark <rnvandemark@gmail.com>
2021-05-07 19:06:31 -0400

    Have program orchestrator collect paths and publish a final collection.

--- src/terple_management/nodes/DecentralizedProgramOrchestrator.py
+++ src/terple_management/nodes/DecentralizedProgramOrchestrator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from terple_msgs.msg import DecentralizedProgram, DecentralizedRobotStatus, Vector2
+from terple_msgs.msg import DecentralizedProgram, DecentralizedRobotStatus, DecentralizedCombinedPaths, Vector2
 from std_msgs.msg import Empty
 from sys import argv as sargv
 
@@ -15,15 +15,15 @@ class ProgramOrchestrator(object):
 
     # Initialize with no entries
     def __init__(self, robot_ids):
-        self.updates_by_id = {r:[False,False] for r in robot_ids}
+        self.updates_by_id = {r:[False,False,None] for r in robot_ids}
 
     # Clear the update flags
     def clear_updates(self):
-        self.updates_by_id = {r:[False,v[1]] for r,v in self.updates_by_id.items()}
+        self.updates_by_id = {r:[False,v[1],v[2]] for r,v in self.updates_by_id.items()}
 
     # Update the robot update and finished flags for the given ID
-    def set_for_id(self, i, finished):
-        self.updates_by_id[i] = [True, finished]
+    def set_for_id(self, i, finished, path):
+        self.updates_by_id[i] = [True, finished, path]
 
     # Check if all robots have been updated this cycle
     def all_ids_updated(self):
@@ -33,15 +33,22 @@ class ProgramOrchestrator(object):
     def all_ids_finished(self):
         return all(v[1] for v in self.updates_by_id.values())
 
+    # Check if all robots have finished their motion
+    def gen_combined_paths(self):
+        paths = [v[2] for v in self.updates_by_id.values()]
+        return DecentralizedCombinedPaths(robot_ids=list(self.updates_by_id.keys()), paths=paths)
+
 # Set the update flag in the given orchestrator
-def update_callback(msg, orch, pub):
-    orch.set_for_id(msg.robot_id, msg.finished)
+def update_callback(msg, orch, step_pub, combined_paths_pub):
+    orch.set_for_id(msg.robot_id, msg.finished, msg.path)
     if orch.all_ids_finished():
+        combined_paths_pub.publish(orch.gen_combined_paths())
         rospy.loginfo("All robots have finished.")
-        rospy.signal_shutdown("Received path, ready for clean shutdown.")
+        rospy.sleep(1)
+        rospy.signal_shutdown("Received path and published combined paths, ready for clean shutdown.")
     elif orch.all_ids_updated():
         orch.clear_updates()
-        pub.publish(Empty())
+        step_pub.publish(Empty())
 
 def main():
     # Capture user input
@@ -66,10 +73,15 @@ def main():
         Empty,
         queue_size=1
     )
+    combined_paths_pub = rospy.Publisher(
+        "/terple/decentralized/combined_paths",
+        DecentralizedCombinedPaths,
+        queue_size=1
+    )
     characteristics_updates_sub = rospy.Subscriber(
         "/terple/decentralized/robot_status",
         DecentralizedRobotStatus,
-        lambda m: update_callback(m, orch, step_pub),
+        lambda m: update_callback(m, orch, step_pub, combined_paths_pub),
         queue_size=100
     )
     programs_pub = rospy.Publisher(
